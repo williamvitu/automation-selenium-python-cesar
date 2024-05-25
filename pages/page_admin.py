179ab13aba5a3778df6be96d42d9548ea6558840
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-05-25 15:11:14 -0300

    Criando metodo - clicar_menu_adm

--- pages/page_admin.py
+++ pages/page_admin.py
@@ -1,2 +1,14 @@
+from selenium.webdriver.common.by import By
 
 
+from util import constants
+
+from pages.page_object import PageObject
+
+class AdminPage(PageObject):
+
+    def __init__(self, driver):
+        super(AdminPage, self).__init__(driver=driver)
+
+    def clicar_menu_adm(self):
+        self.driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a').click()
--- tests/test_aba_admin.py
+++ tests/test_aba_admin.py
@@ -1,25 +1,27 @@
 
 import pytest
 import logging
-import time
+from time import sleep
 
 from selenium.webdriver.support     import expected_conditions as ec
 
 from util import constants
-from pages.page_login import LoginPage
+from pages.page_admin import AdminPage
 
-@pytest.fixture(scope="class", autouse=True)
-def before_after_all():
-    logging.info(f"[SETUP] Starting Suite:   {__name__}")
-    yield 
-    logging.info(f"[TEARDOWN] Starting Suite:   {__name__}")
+# @pytest.fixture(scope="class", autouse=True)
+# def before_after_all():
+#     logging.info(f"[SETUP] Starting Suite:   {__name__}")
+#     yield 
+#     logging.info(f"[TEARDOWN] Starting Suite:   {__name__}")
 
 class TestAbaAdmin:
 
     def test_login_page_success_for_standard_user(self, logged_setup):
         driver = logged_setup
-        time.sleep(10)
-        
+        page_admin = AdminPage(driver)
+        sleep(5)
+        page_admin.clicar_menu_adm()
+        sleep(5)
         
         
     
