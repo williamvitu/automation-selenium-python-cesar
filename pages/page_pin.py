4643153a3c64b850ce4078477600e1b741dc4619
vitourwd <vitorwilliamd@gmail.com>
2024-05-29 00:05:42 -0300

    novo teste, novos locators

--- pages/page_pin.py
+++ pages/page_pin.py
@@ -1,3 +1,4 @@
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,21 +6,33 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from util import constants
 from pages.page_object import PageObject
-from faker import Faker
 
 import random
 import logging
 import time
+
 class PinPage(PageObject):
         
     base_url = f"{constants.BASE_URL}/pim/viewEmployeeList"
     
     # PIM - MAIN PAGE
+    panel_employee_details = (By.XPATH, '//div[contains(@class, "orangehrm-edit-employee-content")]')
+    panel_employee_loader = (By.CLASS_NAME, 'oxd-form-loader') 
+    
     table_employee_card_list  = (By.CSS_SELECTOR, '[role="row"]')
-    input_employee_name_filter = (By.XPATH, '//input[@placeholder="Type for hints..."]')  
-    input_employee_supervisor_name_filter = (By.CSS_SELECTOR, '[class="oxd-autocomplete-text-input oxd-autocomplete-text-input--active"]')
+    
+    input_employee_name_filter = (By.XPATH, '//*[@class="oxd-autocomplete-text-input oxd-autocomplete-text-input--active"]//input')  
+    input_employee_id = (By.CSS_SELECTOR,'input.oxd-input.oxd-input--active:not([placeholder]):empty')
+    input_employee_supervisor_name_filter = (By.CSS_SELECTOR, '.oxd-autocomplete-text-input.oxd-autocomplete-text-input--active input[placeholder]')
+    input_employee_list = (By.CSS_SELECTOR, '[class="oxd-input-group oxd-input-field-bottom-space"]')
+    
+    button_reset = (By.XPATH, '//button[normalize-space()="Reset"]')
+    
     dropdown_list = (By.CSS_SELECTOR, '[class="oxd-icon bi-caret-down-fill oxd-select-text--arrow"]')
+    dropdown_options = (By.CSS_SELECTOR, '[role="option"] span')
 
+    list_employement_status = (By.CSS_SELECTOR, '[role="listbox"]')
+    
     # PIM - EMPLOYEE PROFILE DETAILS
     label_employee_name = (By.CSS_SELECTOR,'h6[class="oxd-text oxd-text--h6 --strong"]')
     input_employee_name = (By.NAME, 'firstName')
@@ -35,39 +48,60 @@ class PinPage(PageObject):
     def send_keys_on_employee_name_filter(self, text: str):
         self.driver.find_element(*self.input_employee_name_filter).send_keys(text)
     
-    def send_keys_on_employee_supervisor_name_filter(self, text: str):
-        self.driver.find_element(*self.input_employee_supervisor_name_filter).send_keys(text)
+    def send_keys_on_employee_id(self, text: str):
+        self.driver.find_element(*self.input_employee_id).send_keys(text)
 
+    def send_keys_on_employee_supervisor_name(self, text: str):
+        self.driver.find_elements(*self.input_employee_name_filter)[1].send_keys(text)
+
+    def click_and_select_option_on_employement_status(self):
+        self.driver.find_elements(*self.input_employee_list)[2].click()
+        self.select_any_option_dropdown()
+
+    def click_on_reset_button(self):
+        self.driver.find_element(*self.button_reset).click()
+
+    def click_and_select_option_on_include(self):
+        self.driver.find_elements(*self.input_employee_list)[3].click()
+        self.select_any_option_dropdown()
+
+    def click_and_select_option_on_title(self):
+        self.driver.find_elements(*self.input_employee_list)[5].click()
+        self.select_any_option_dropdown()
+
+    def click_and_select_option_on_sub_unit(self):
+        self.driver.find_elements(*self.input_employee_list)[6].click()
+        self.select_any_option_dropdown()
 
     def click_on_edit_employee_button(self):
         cards_list = self.driver.find_elements(*self.table_employee_card_list)
         card = random.choice(cards_list)
         card.click()
 
+    def select_any_option_dropdown(self):
+        employements =  self.driver.find_elements(*self.dropdown_options)
+        employement = random.choice(employements)
+        employement.click()
+
     def get_employee_name(self):
-        # Esse wait também não funciona, incluí um time.sleep
+        # Tentei vários wait's, por algum motivo não funcionam, deixei o time.sleep
         # employee_name = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.label_employee_name))
         time.sleep(10)
         return self.driver.find_element(*self.label_employee_name).text
 
+
     def edit_employee_name(self):
         
-        # Esse Wait não funciona
-        e = WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable(self.input_employee_name))
-        # Deixei esse time.sleep também
-        time.sleep(5)
+        WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located(self.panel_employee_loader))
+
+        e = self.driver.find_element(*self.input_employee_name)
+
         e.send_keys("TEST")
         
-        # Esse Wait também não funciona
-        buttons = WebDriverWait(self.driver, 15).until(EC.visibility_of_all_elements_located(self.buttons_save_list))
-        
-        # Tentei de vários jeitos talvez seja bug no componente, não sei, deixei o teste aqui mais para conhecimento
-        # Sem os 'time.sleep' nada funciona nessa página
-        time.sleep(5)
+        buttons = self.driver.find_elements(*self.buttons_save_list)
+
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(buttons[0]))
 
         buttons[0].click()
-        time.sleep(10)
-        # Este wait abaixo também não funciona, impressionante, deixei o time.sleep acima
-        # employee_name = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.label_employee_name))
-
+ 
         
--- tests/test_aba_pin.py
+++ tests/test_aba_pin.py
@@ -5,19 +5,26 @@ import logging
 from pages.page_pin import PinPage
 
 class TestAbaPin:
-
+    
+    @pytest.mark.pin
     def test_edit_employee_name(self, logged_setup):
         page_pin = PinPage(logged_setup)
         page_pin.go_to_pin_menu()
         page_pin.click_on_edit_employee_button()
         page_pin.edit_employee_name()
-        logging.info(page_pin.get_employee_name())
+        page_pin.driver.refresh()
         assert "TEST" in page_pin.get_employee_name()
 
-    @pytest.mark.pin
+    @pytest.mark.pin1
     def test_reset_search(self, logged_setup):
         page_pin = PinPage(logged_setup)
         page_pin.go_to_pin_menu()
         page_pin.send_keys_on_employee_name_filter("vitor")
-        page_pin.send_keys_on_employee_supervisor_name_filter("teste")
-        time.sleep(5)
+        page_pin.send_keys_on_employee_id("vitor")
+        page_pin.send_keys_on_employee_supervisor_name('vitor')
+        page_pin.click_and_select_option_on_employement_status()
+        page_pin.click_and_select_option_on_include()
+        page_pin.click_and_select_option_on_sub_unit()
+        page_pin.click_and_select_option_on_title()
+        page_pin.click_on_reset_button()
+        
